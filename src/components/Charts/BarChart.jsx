41c157ff0a08bbfabccf33a3a8eea0e713fd9fdd
QuentinHeinis <94958477+QuentinHeinis@users.noreply.github.com>
2022-08-10 00:02:51 +0200

    début barchart mano

--- src/components/Charts/BarChart.jsx
+++ src/components/Charts/BarChart.jsx
@@ -1,50 +1,50 @@
 import React from 'react'
-import Plot from 'react-plotly.js'
 
 
 const BarChart = () => {
-  var x = ['janvier', 'fevrier', 'mars', 'avril', 'mai', 'juin', 'juillet', 'aout', 'septembre', 'octobre', 'novembre', 'decembre']
+  var mois = ['jan', 'fev', 'mars', 'avril', 'mai', 'juin', 'juil', 'aout', 'sept', 'oct', 'nov', 'dec']
 
-  var y = ['0','0','0','0','0','0','294','3','0','3','0','3','0',]
+  var revenu = ['0','0','1200','0','0','0','294','3','0','500','0','3',]
   var z = ['0','0','0','0','0','0','100','3','0','3','0','3','0',]
   var w = ['0','0','0','0','0','0','194','3','0','3','0','3','0',]
+  let totalHeight = 0.8 * 500
+  let height = (600*totalHeight) / 1200
+  console.log(height)
+  let config = { responsive: true}
+  
   return (
-    <div>
-      <Plot
-          data = {[
-          {
-            histfunc: "sum",
-            y,
-            x,
-            type: "histogram",
-            name: "revenus",
-            marker:{
-              color:'rgb(54,127,180)'
-            }
-          },
-          {
-            histfunc: "sum",
-            y:z,
-            x,
-            type: "histogram",
-            name: "dépenses",
-            marker:{
-              color:'rgb(123,33,76)'
-            }
-          },
-          {
-            histfunc: "sum",
-            y:w,
-            x,
-            type: "histogram",
-            name: "économies",
-            marker:{
-              color:'rgb(57,180,54)'
-            }
-          }]}
-          className="w-full rounded-xl overflow-hidden"
-          />
-    </div>
+        <div className="w-[1500px] h-[500px] bg-red-50 relative mx-auto my-11 rounded-xl">
+          <div className=' h-[80%] w-[80%] relative top-[10%] left-[10%] border-black border-b'>
+            <div className='relative flex justify-between top-full'>
+              {mois.map((item) => (
+                <div className='relative top-full h-fit w-10 text-center'>{item}</div>
+              ))}
+            </div>
+            <div className='absolute flex justify-between w-full bottom-0 z-10 -scale-y-100'>
+              {revenu.map((item) => (
+                <div className='relative top-full w-10 text-center bg-red-600' style={{height:(item*totalHeight) / 1200+'px'}}>
+                </div>
+              ))}
+            </div>
+
+            {/* range */}
+            <div className='absolute top-[calc(100%-12px)] -left-12 h-fit w-10 text-right'>0</div>
+            <div className='absolute top-[calc(75%-12px)] -left-12 h-fit w-10 text-right'>300</div>
+            <div className='absolute top-[calc(50%-12px)] -left-12 h-fit w-10 text-right'>600</div>
+            <div className='absolute top-[calc(25%-12px)] -left-12 h-fit w-10 text-right'>900</div>
+            <div className='absolute top-[-12px] -left-12 h-fit w-10 text-right'>1200</div>
+            
+            {/* line */}
+
+            <div className='absolute top-[75%]  h-[2px] w-full bg-[#D1D1D1] text-right'></div>
+            <div className='absolute top-[50%]  h-[2px] w-full bg-[#D1D1D1] text-right'></div>
+            <div className='absolute top-[25%]  h-[2px] w-full bg-[#D1D1D1] text-right'></div>
+            <div className='absolute top-0  h-[2px] w-full bg-[#D1D1D1] text-right'></div>
+
+
+          </div>
+        </div>        
+
   )
 }
 
--- src/pages/DashBoard/Dashboard.jsx
+++ src/pages/DashBoard/Dashboard.jsx
@@ -3,7 +3,7 @@ import { TrendingDownIcon, TrendingUpIcon, LibraryIcon, PlusCircleIcon } from '@
 import { BarChart } from '../../components'
 
 // import { useStateContext } from '../../contexts/ContextProvider'clea
-const MoneyCard = ({mainColor, lightColor, darkColor, icon, title, text}) => (<div className='w-full max-w-md h-64 rounded-xl flex flex-col overflow-hidden'>
+const MoneyCard = ({mainColor, lightColor, darkColor, icon, title, text}) => (<div className='w-full h-64 rounded-xl flex flex-col overflow-hidden'>
   <div className='h-1/2 items-center flex pl-5 relative' style={{backgroundColor:mainColor}}>
     <p className='text-white text-2xl relative z-10'>
       {title}
@@ -32,13 +32,13 @@ const Dashboard = () => {
           Ajouter une transaction <PlusCircleIcon className='h-5 w-5'/>
         </span>
       </div>
-      <div className='flex justify-center gap-2 flex-wrap gap-y-4'>
+      <div className='grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-9 flex-wrap gap-y-4'>
         <MoneyCard mainColor='#367FB4' darkColor='#245578' lightColor='#6AA6D1' text='1250' title='Total revenus' icon={<TrendingUpIcon className='h-10 w-10'/>}></MoneyCard>
         <MoneyCard mainColor='#7B214C' darkColor='#4E0E2C' lightColor='#B95284' text='1250' title='Total dépenses' icon={<TrendingDownIcon className='h-10 w-10'/>}></MoneyCard>
         <MoneyCard mainColor='#39B436' darkColor='#0D540B' lightColor='#85DB83' text='1250' title='Total économies' icon={<LibraryIcon className='h-10 w-10'/>}></MoneyCard>
       </div>
-      <div>
-        <BarChart></BarChart>
+      <div className='h-fit'>
+        <BarChart className="w-full h-[400px]"/>
       </div>
     </div>
   )
